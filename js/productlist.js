26430116fb7a4bd1618324c722dc8c61de27d9b8
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-02-11 17:37:16 +0100

    backtoprevious

--- js/productlist.js
+++ js/productlist.js
@@ -1,5 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category");
+
 console.log(category);
 
 const url = "https://kea-alt-del.dk/t7/api/products?category=" + category;
