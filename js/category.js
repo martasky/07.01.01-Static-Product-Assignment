44f8171c7db6db9defaabd41c29638a60c3ad2ff
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-02-14 09:38:46 +0100

    remove commented out code

--- js/category.js
+++ js/category.js
@@ -23,38 +23,3 @@ function showCategory(category) {
   const parent = document.querySelector(".categories ul");
   parent.appendChild(clone);
 }
-
-// document.querySelector("ul .brand").addEventListener("click", handleBrands);
-
-// const urlBrands = "https://kea-alt-del.dk/t7/api/brands";
-// fetch(urlBrands)
-//   .then(function (res2) {
-//     return res2.json();
-//   })
-//   .then(function (branddata) {
-//     handleBrands(branddata);
-//   });
-// function handleBrands(branddata) {
-//   console.log(branddata);
-//   branddata.forEach(showBrand);
-// }
-// function showBrand(brand) {
-//   console.log(brand);
-
-//   const template = document.querySelector("#brands").content;
-//   const copy = template.cloneNode(true);
-//   copy.querySelector("li a").textContent = brand.brandname;
-//   copy.querySelector(
-//     "li a"
-//   ).href = `productlist.html?brandname=${brand.brandname}`;
-//   const parent = document.querySelector("section");
-//   parent.appendChild(copy);
-// }
-
-// const urlParams = new URLSearchParams(window.location.search);
-// const brand = urlParams.get("brand");
-// if (brand === "yes") {
-//   document.querySelector(".categories").remove();
-// } else {
-//   showCategory(category);
-// }
